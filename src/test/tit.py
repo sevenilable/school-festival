cd300882d94c09c3c70f7907d852d55c99501df1
sevenilable <132343337+sevenilable@users.noreply.github.com>
2023-05-18 16:19:52 +0900

    Update titl.py

--- src/test/tit.py
+++ src/test/tit.py
@@ -229,7 +229,7 @@ class Enemy3Bullet:
 class App:
     def __init__(self):
         pyxel.init(128, 128, title="Pyxel Platformer")
-        pyxel.load("platformer (2).pyxres")
+        pyxel.load("platformer(2).pyxres")
 
         # Change enemy spawn tiles invisible
         pyxel.image(0).rect(0, 8, 24, 8, TRANSPARENT_COLOR)
