09ddfaafd4d4811b82a5634dfb2434ad572f587b
sevenilable <132343337+sevenilable@users.noreply.github.com>
2023-06-02 12:45:37 +0900

    Update and rename 2023to-to-sai-Level3a.py to 2023to-to-sai-Level3b.py

--- src/test/2023to-to-sai-Level3b.py
+++ src/test/2023to-to-sai-Level3b.py
@@ -233,7 +233,7 @@ class App:
     def __init__(self):
         pyxel.init(128, 128, title="Pyxel Platformer")
         pyxel.load("platform (1) (1) (1) (1) (1) (1).pyxres")
-        self.scene = SCENE_TITLE
+        self.scene = SCENE_PLAY
         self.score = 0
         self.score2 = 0
         self.highscore = 0
@@ -276,7 +276,7 @@ class App:
         cleanup_list(enemies)
         
     def update_title_scene(self):
-            self.scene = SCENE_TITLE
+            self.scene = SCENE_PLAY
         
 
     def update_gameover_scene(self):
